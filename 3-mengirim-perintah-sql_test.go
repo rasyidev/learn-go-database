fbaae7a5ff8945a8e9d334b4a47d865d0fc4d2b5
rasyidev <habib.rasyid11@gmail.com>
2022-09-11 20:53:24 +0700

    Menambahkan demo perintah SELECT pada SQL

--- 3-mengirim-perintah-sql_test.go
+++ 3-mengirim-perintah-sql_test.go
@@ -20,3 +20,36 @@ func TestPerintahSQL(t *testing.T) {
 	fmt.Println("Berhasil insert data ke dalam database")
 
 }
+
+func TestPerintahSelect(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	sqlQuery := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, sqlQuery)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(id, "|", name)
+	}
+}
+
+/*
+=== RUN   TestPerintahSelect
+rasyidev | Rasyidev
+taeri | Kim Taeri
+yoona | Im Yoona
+--- PASS: TestPerintahSelect (0.02s)
+PASS
+ok      learn-go-database       0.073s
+*/
